c3fa7119579c837028d3a4592cb7be4101bd8c4a
Malik <bittiev.malik@mail.ru>
2025-02-21 02:19:59 +0300

    fix litle

--- app/src/main/java/com/example/barterly/model/Offer.kt
+++ app/src/main/java/com/example/barterly/model/Offer.kt
@@ -3,21 +3,21 @@ package com.example.barterly.model
 import java.io.Serializable
 
 data class Offer(
-    val title :String? = null,
-    val country:String? = null,
-    val city:String? = null,
-    val phone:String? = null,
-    val adress:String? = null,
-    val category:String? = null,
-    val price:String? = null,
-    val description:String? = null,
-    val key:String? = null,
-    val uid:String? = null,
-    var isFav:Boolean = false,
+    val title: String? = null,
+    val country: String? = null,
+    val city: String? = null,
+    val phone: String? = null,
+    val adress: String? = null,
+    val category: String? = null,
+    val price: String? = null,
+    val description: String? = null,
+    val key: String? = null,
+    val uid: String? = null,
+    var isFav: Boolean = false,
     var favCounter: String = "0",
 
-    var viewcounter:String = "0",
-    var emailcounter:String = "0",
-    var callscounter:String = "0"
+    var viewcounter: String = "0",
+    var emailcounter: String = "0",
+    var callscounter: String = "0"
 
-): Serializable
+) : Serializable
